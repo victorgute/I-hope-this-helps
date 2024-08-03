808ec5c2194d88deba7f85ee0d084c40bbcef93b
victor <victor.h.gutierrez.carvalho>
2024-08-03 01:13:50 -0300

    kjsjdka

--- script.js
+++ script.js
@@ -1,26 +1,23 @@
 document.addEventListener('DOMContentLoaded', () => {
     const startBtn = document.getElementById('start-btn');
     const nextBtn = document.getElementById('next-btn');
-    const pageStart = document.getElementById('page-start');
-    const pageIntro = document.getElementById('page-intro');
-    const pageSurprises = document.getElementById('page-surprises');
-    const surpriseBtns = Array.from(document.querySelectorAll('.surprise-btn'));
-
+    const surpriseBtns = document.querySelectorAll('.surprise-btn');
+    
     startBtn.addEventListener('click', () => {
-        pageStart.style.display = 'none';
-        pageIntro.style.display = 'block';
+        document.getElementById('page-start').classList.remove('active');
+        document.getElementById('page-intro').classList.add('active');
     });
-
+    
     nextBtn.addEventListener('click', () => {
-        pageIntro.style.display = 'none';
-        pageSurprises.style.display = 'block';
+        document.getElementById('page-intro').classList.remove('active');
+        document.getElementById('page-surprises').classList.add('active');
     });
 
     surpriseBtns.forEach(btn => {
         btn.addEventListener('click', (e) => {
-            const id = e.target.innerHTML.split(' ')[1];
-            document.querySelectorAll('.page').forEach(page => page.style.display = 'none');
-            document.getElementById(`page-detail${id}`).style.display = 'block';
+            const targetPage = e.target.getAttribute('data-target');
+            document.querySelectorAll('.page').forEach(page => page.classList.remove('active'));
+            document.getElementById(targetPage).classList.add('active');
         });
     });
 });
